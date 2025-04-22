670c9d46bcb16392045d9ce6b496b64a8fcd3b4f
Luana Cristina da Silva <92609041+Luanacsilva@users.noreply.github.com>
2025-04-22 08:52:18 -0300

    Delete modelos directory

--- modelos/modelo_jogador.py
+++
@@ -1,8 +0,0 @@
-def validar_jogador(jogador):
-    campos = ["nickname", "trofeus", "nivel", "tempo_jogo_meses"]
-    for campo in campos:
-        if campo not in jogador:
-            print(f"❌ Jogador inválido: faltando campo {campo}")
-            return False
-    return True
-0
